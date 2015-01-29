c554fda782128bf624524a167d69c0388bd70ed2
Jorrobo <lynch446@morris.umn.edu>
2015-01-29 13:45:31 -0600

    pushing major changes from controller and GPA calc.

--- public/javascript/main.controller.js
+++ public/javascript/main.controller.js
@@ -48,3 +48,73 @@ var mainApp = angular.module("mainApp", []);
            return $scope.data.length;
        };
     });
+
+//==================== GPA CONTROLLER ==================================
+    mainApp.controller('GPACtrl', function($scope){
+        console.log("controller loaded!");
+
+        $scope.textField = "";
+
+        //try to store the data first.
+        $scope.data = [
+
+        ]
+
+        $scope.addData = function(){
+            if($scope.textField == "A") {
+                $scope.data.push({text: $scope.textField});
+                $scope.textField = "";
+            }
+            if($scope.textField == "A-"){
+                $scope.data.push({text: $scope.textField});
+                $scope.textField = "";
+            }
+            if($scope.textField == "B+"){
+                $scope.data.push({text: $scope.textField});
+                $scope.textField = "";
+            }
+            if($scope.textField == "B"){
+                $scope.data.push({text: $scope.textField});
+                $scope.textField = "";
+            }
+            if($scope.textField == "B-"){
+                $scope.data.push({text: $scope.textField});
+                $scope.textField = "";
+            }
+            if($scope.textField == "C+"){
+                $scope.data.push({text: $scope.textField});
+                $scope.textField = "";
+            }
+            if($scope.textField == "C"){
+                $scope.data.push({text: $scope.textField});
+                $scope.textField = "";
+            }
+            if($scope.textField == "C-"){
+                $scope.data.push({text: $scope.textField});
+                $scope.textField = "";
+            }
+            if($scope.textField == "D+"){
+                $scope.data.push({text: $scope.textField});
+                $scope.textField = "";
+            }
+            if($scope.textField == "D"){
+                $scope.data.push({text: $scope.textField});
+                $scope.textField = "";
+            }
+            if($scope.textField == "D-"){
+                $scope.data.push({text: $scope.textField});
+                $scope.textField = "";
+            }
+            if($scope.textField == "F"){
+                $scope.data.push({text: $scope.textField});
+                $scope.textField = "";
+            }
+            else{
+                $scope.data.push({text: $scope.textField});
+                $scope.textField = "";
+            }
+        }
+        $scope.removeData = function(index){
+            $scope.data.splice(index, 1);
+        };
+    });
