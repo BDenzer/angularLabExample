a25b9b8b07a165cc2d5291f82c60285691bc62a8
BDenzer <denz0045@morris.umn.edu>
2015-02-03 22:43:54 -0600

    Made significant changes that fix some bugs and added some bugs, not sure if it's better or not.

--- public/javascript/main.controller.js
+++ public/javascript/main.controller.js
@@ -59,7 +59,15 @@ var mainApp = angular.module("mainApp", []);
 
         $scope.textFieldCredits = "";
 
-        $scope.data = [
+        $scope.grades = [
+
+        ];
+
+        $scope.classes = [
+
+        ];
+
+        $scope.credits = [
 
         ];
 
@@ -73,71 +81,73 @@ var mainApp = angular.module("mainApp", []);
 
         $scope.addGrades = function(){
             if($scope.textFieldGrades == "A") {
-                $scope.temp.push(4.0);
+                $scope.grades.push(4.0);
                 $scope.textFieldGrades = "";
             }
             else if($scope.textFieldGrades == "B"){
-                $scope.temp.push(3.0);
+                $scope.grades.push(3.0);
                 $scope.textFieldGrades = "";
             }
             else if($scope.textFieldGrades == "C"){
-                $scope.temp.push(2.0);
+                $scope.grades.push(2.0);
                 $scope.textFieldGrades = "";
             }
             else if($scope.textFieldGrades == "D"){
-                $scope.temp.push(1.0);
+                $scope.grades.push(1.0);
                 $scope.textFieldGrades = "";
             }
             else if($scope.textFieldGrades == "F"){
-                $scope.temp.push(0);
+                $scope.grades.push(0);
                 $scope.textFieldGrades = "";
             }
             //getting classes
             if($scope.textFieldClasses.length >= 1) {
-                $scope.temp.push($scope.textFieldClasses);
+                $scope.classes.push($scope.textFieldClasses);
                 $scope.textFieldClasses = "";
             }
             //getting credits
             if($scope.textFieldCredits.length >= 1) {
                 if($scope.textFieldCredits == "1") {
-                    $scope.temp.push(1.0);
+                    $scope.credits.push(1.0);
                     $scope.total = $scope.total + 1.0;
                     $scope.textFieldCredits = "";
                 }
                 else if($scope.textFieldCredits == "2") {
-                    $scope.temp.push(2.0);
+                    $scope.credits.push(2.0);
                     $scope.total = $scope.total + 2.0;
                     $scope.textFieldCredits = "";
                 }
                 else if($scope.textFieldCredits == "3") {
-                    $scope.temp.push(3.0);
+                    $scope.credits.push(3.0);
                     $scope.total = $scope.total + 3.0;
                     $scope.textFieldCredits = "";
                 }
                 else if($scope.textFieldCredits == "4") {
-                    $scope.temp.push(4.0);
+                    $scope.credits.push(4.0);
                     $scope.total = $scope.total + 4.0;
                     $scope.textFieldCredits = "";
                 }
                 else if($scope.textFieldCredits == "5") {
-                    $scope.temp.push(5.0);
+                    $scope.credits.push(5.0);
                     $scope.total = $scope.total + 5.0;
                     $scope.textFieldCredits = "";
                 }
             }
-            $scope.data.push({text: $scope.temp});
-            $scope.temp = [];
+
+
 
         };
 
         $scope.removeData = function(index){
-            $scope.data.splice(index, 1);
+            $scope.grades.splice(index, 1);
+            $scope.classes.splice(index, 1);
+            $scope.credits.splice(index, 1);
         };
 
         $scope.getGPA = function(){
             var GPT = 0;
-            for (i = 0; i < data.length; i++){
-                GPT = GPT + (data[i[0]] * data[i[2]]);
+            for (i = 0; i < $scope.grades.length; i++){
+                GPT = GPT + ($scope.grades[i]*$scope.credits[i]);
             }
             $scope.GPA = GPT / $scope.total;
             return $scope.GPA;
